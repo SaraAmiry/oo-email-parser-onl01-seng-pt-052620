749487416c98393663a536e59e151bac3b7c37c7
Sara Amiry <saraamiry24@gmail.com>
2020-07-01 19:15:49 +0000

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,4 +1,17 @@
 # Build a class EmailAddressParser that accepts a string of unformatted 
+#require "pry"
+class EmailAddressParser
+  e = []
+  attr_accessor :emails
+  
+  def initialize(emails)
+    @emails = emails
+  end 
+  
+  def parse
+    @emails.split(/[, ]+/).uniq 
+  end 
+end 
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
